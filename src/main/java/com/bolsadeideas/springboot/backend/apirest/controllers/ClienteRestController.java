ff2c2fa04e650f06e02a37f8a37b2f10c125d408
unknown <jsuegr@gmail.com>
2020-12-31 15:43:48 -0600

    44. Escribiendo los métodos show y create en el Controlador Backend API Rest

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -1,11 +1,17 @@
 package com.bolsadeideas.springboot.backend.apirest.controllers;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.models.Cliente;
@@ -24,4 +30,17 @@ public class ClienteRestController {
 	public List<Cliente> index(){
 		return clienteService.findAll(); 
 	}
+	
+	@GetMapping("/clientes/{id}") 
+	public Cliente show(@PathVariable Long id) {
+		return clienteService.findById(id);
+	}
+	
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return clienteService.save(cliente);
+	}
+	
+	
 }
